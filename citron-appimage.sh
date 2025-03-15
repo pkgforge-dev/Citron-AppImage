08b80235eada5f070bedd05a79ba2419f5d98cc6
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-03-15 05:36:58 -0400

    add `libxss`

--- citron-appimage.sh
+++ citron-appimage.sh
@@ -89,6 +89,7 @@ xvfb-run -a -- ./lib4bin -p -v -e -s -k \
 	/usr/lib/libEGL* \
 	/usr/lib/dri/* \
 	/usr/lib/libvulkan* \
+	/usr/lib/libXss.so* \
 	/usr/lib/qt6/plugins/audio/* \
 	/usr/lib/qt6/plugins/bearer/* \
 	/usr/lib/qt6/plugins/imageformats/* \
