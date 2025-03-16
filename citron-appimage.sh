057eaf7226885720c0a27c70d6a1c877f405921f
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-03-16 08:17:48 -0400

    use mirror if upstream repo fails to clone

--- citron-appimage.sh
+++ citron-appimage.sh
@@ -19,8 +19,12 @@ else
 fi
 UPINFO="gh-releases-zsync|$(echo "$GITHUB_REPOSITORY" | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
 
-# BUILD CITRON
-git clone https://git.citron-emu.org/Citron/Citron.git ./citron
+# BUILD CITRON, fallback to mirror if upstream repo fails to clone
+if ! git clone 'https://git.citron-emu.org/Citron/Citron.git' ./citron; then
+	echo "Using mirror instead..."
+	rm -rf ./citron || true
+	git clone 'https://github.com/pkgforge-community/git.citron-emu.org-Citron-Citron.git' ./citron
+fi
 
 ( cd ./citron
 	if [ "$DEVEL" = 'true' ]; then
