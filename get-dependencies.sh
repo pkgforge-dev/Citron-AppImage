63e44c95f00157fdc59744eb9b38133cc382921e
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-02 19:06:52 -0400

    test

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -15,8 +15,6 @@ pacman -Syu --noconfirm \
 	dav1d               \
 	doxygen             \
 	enet                \
-	ffmpeg              \
-	ffmpeg4.4           \
 	fmt                 \
 	gamemode            \
 	gcc                 \
@@ -83,7 +81,6 @@ case "$ARCH" in
 esac
 
 LLVM_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/llvm-libs-nano-$PKG_TYPE"
-FFMPEG_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/ffmpeg-mini-$PKG_TYPE"
 QT6_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/qt6-base-iculess-$PKG_TYPE"
 LIBXML_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/libxml2-iculess-$PKG_TYPE"
 OPUS_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/opus-nano-$PKG_TYPE"
@@ -93,7 +90,6 @@ echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$LLVM_URL"   -O ./llvm-libs.pkg.tar.zst
 wget --retry-connrefused --tries=30 "$QT6_URL"    -O ./qt6-base-iculess.pkg.tar.zst
 wget --retry-connrefused --tries=30 "$LIBXML_URL" -O ./libxml2-iculess.pkg.tar.zst
-wget --retry-connrefused --tries=30 "$FFMPEG_URL" -O ./ffmpeg-mini.pkg.tar.zst
 wget --retry-connrefused --tries=30 "$OPUS_URL"   -O ./opus-nano.pkg.tar.zst
 
 pacman -U --noconfirm ./*.pkg.tar.zst
