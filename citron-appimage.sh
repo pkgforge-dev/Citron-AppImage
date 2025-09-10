31f3b7420850e134d0bc615b8616be267fb6df51
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-09-10 05:47:42 +0000

    temp fix™ using AUR package to build citron (#61)

--- citron-appimage.sh
+++ citron-appimage.sh
@@ -3,74 +3,9 @@
 set -ex
 
 ARCH="$(uname -m)"
-
 if [ "$1" = 'v3' ] && [ "$ARCH" = 'x86_64' ]; then
-	echo "Making x86-64-v3 optimized build of citron..."
 	ARCH="${ARCH}_v3"
-	ARCH_FLAGS="-march=x86-64-v3 -O3"
-elif [ "$ARCH" = 'x86_64' ]; then
-	echo "Making x86-64 generic build of citron..."
-	ARCH_FLAGS="-march=x86-64 -mtune=generic -O3"
-else
-	echo "Making aarch64 build of citron..."
-	ARCH_FLAGS="-march=armv8-a -mtune=generic -O3"
 fi
-
-# BUILD CITRON, fallback to mirror if upstream repo fails to clone
-git clone --recursive "https://git.citron-emu.org/citron/emulator.git" ./citron && (
-	cd ./citron
-	if [ "$DEVEL" = 'true' ]; then
-		CITRON_TAG="$(git rev-parse --short HEAD)"
-		echo "Making nightly \"$CITRON_TAG\" build"
-		VERSION="$CITRON_TAG"
-	else
-		CITRON_TAG=$(git describe --tags)
-		echo "Making stable \"$CITRON_TAG\" build"
-		git checkout "$CITRON_TAG"
-		VERSION="$(echo "$CITRON_TAG" | awk -F'-' '{print $1}')"
-	fi
-
-	# Upstream fixed this issue, but a newer version of boost came out and broke it again 🤣
-	find . -type f \( -name '*.cpp' -o -name '*.h' \) | xargs sed -i 's/\bboost::asio::io_service\b/boost::asio::io_context/g'
-	find . -type f \( -name '*.cpp' -o -name '*.h' \) | xargs sed -i 's/\bboost::asio::io_service::strand\b/boost::asio::strand<boost::asio::io_context::executor_type>/g'
-	find . -type f \( -name '*.cpp' -o -name '*.h' \) | xargs sed -i 's|#include *<boost/process/async_pipe.hpp>|#include <boost/process/v1/async_pipe.hpp>|g'
-	find . -type f \( -name '*.cpp' -o -name '*.h' \) | xargs sed -i 's/\bboost::process::async_pipe\b/boost::process::v1::async_pipe/g'
-
-	# remove mysterious sse2neon library dependency
-	sed -i '/sse2neon/d' ./src/video_core/CMakeLists.txt
-
-	mkdir build
-	cd build
-	cmake .. -GNinja \
-		-DCITRON_USE_BUNDLED_VCPKG=OFF                \
-		-DCITRON_USE_BUNDLED_QT=OFF                   \
-		-DUSE_SYSTEM_QT=ON                            \
-		-DCITRON_USE_BUNDLED_FFMPEG=OFF               \
-		-DCITRON_USE_BUNDLED_SDL2=ON                  \
-		-DCITRON_USE_EXTERNAL_SDL2=OFF                \
-		-DCITRON_TESTS=OFF                            \
-		-DCITRON_CHECK_SUBMODULES=OFF                 \
-		-DCITRON_USE_LLVM_DEMANGLE=OFF                \
-		-DCITRON_ENABLE_LTO=ON                        \
-		-DCITRON_USE_QT_MULTIMEDIA=OFF                \
-		-DCITRON_USE_QT_WEB_ENGINE=OFF                \
-		-DENABLE_QT_TRANSLATION=ON                    \
-		-DUSE_DISCORD_PRESENCE=OFF                    \
-		-DBUNDLE_SPEEX=ON                             \
-		-DCITRON_USE_FASTER_LD=OFF                    \
-		-DCMAKE_INSTALL_PREFIX=/usr                   \
-		-DCMAKE_CXX_FLAGS="$ARCH_FLAGS -Wno-error -w" \
-		-DCMAKE_C_FLAGS="$ARCH_FLAGS"                 \
-		-DCMAKE_SYSTEM_PROCESSOR="$(uname -m)"        \
-		-DCMAKE_BUILD_TYPE=Release                    \
-		-DCMAKE_POLICY_VERSION_MINIMUM=3.5
-	ninja
-	sudo ninja install
-	echo "$VERSION" >~/version
-)
-rm -rf ./citron
-
-# Deploy AppImage
 VERSION="$(cat ~/version)"
 URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
 SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
@@ -87,7 +22,11 @@ export DEPLOY_PIPEWIRE=1
 # ADD LIBRARIES
 wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
 chmod +x ./quick-sharun
-./quick-sharun /usr/bin/citron* /usr/lib/libgamemode.so*
+./quick-sharun /usr/bin/citron* /usr/lib/libgamemode.so* /usr/lib/libpulse.so*
+
+# TODO: remove me once we can build citron again without qt6-webengine nonsense
+cp -rv /usr/share/qt6/resources    ./AppDir/shared/lib/qt6
+cp -rv /usr/share/qt6/translations ./AppDir/shared/lib/qt6
 
 if [ "$DEVEL" = 'true' ]; then
 	sed -i 's|Name=citron|Name=citron nightly|' ./AppDir/*.desktop
@@ -103,3 +42,8 @@ fi
 wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
 chmod +x ./uruntime2appimage
 ./uruntime2appimage
+
+mkdir -p ./dist
+mv -v ./*.AppImage* ./dist
+mv -v ~/version     ./dist
+echo "All done!"
--- get-dependencies.sh
+++ get-dependencies.sh
@@ -11,26 +11,18 @@ pacman -Syu --noconfirm \
 	catch2              \
 	cmake               \
 	curl                \
-	enet                \
 	fmt                 \
 	gamemode            \
 	gcc                 \
 	git                 \
-	glslang             \
-	glu                 \
-	hidapi              \
-	libdecor            \
-	libvpx              \
 	libxi               \
 	libxkbcommon-x11    \
 	libxss              \
 	mbedtls2            \
 	mesa                \
-	nasm                \
 	ninja               \
-	nlohmann-json       \
-	numactl             \
 	openal              \
+	pipewire-audio      \
 	pulseaudio          \
 	pulseaudio-alsa     \
 	qt6-base            \
@@ -55,4 +47,47 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-common intel-media-driver
+./get-debloated-pkgs.sh --add-mesa qt6-base-mini llvm-libs-nano opus-nano
+
+echo "Building citron..."
+echo "---------------------------------------------------------------"
+sed -i 's|EUID == 0|EUID == 69|g' /usr/bin/makepkg
+sed -i 's|-O2|-O3|; s|MAKEFLAGS=.*|MAKEFLAGS="-j$(nproc)"|; s|#MAKEFLAGS|MAKEFLAGS|' /etc/makepkg.conf
+cat /etc/makepkg.conf
+
+if [ "$1" = 'v3' ] && [ "$ARCH" = 'x86_64' ]; then
+	echo "Making x86-64-v3 optimized build of citron..."
+	ARCH_FLAGS="-march=x86-64-v3 -O3"
+elif [ "$ARCH" = 'x86_64' ]; then
+	echo "Making x86-64 generic build of citron..."
+	ARCH_FLAGS="-march=x86-64 -mtune=generic -O3"
+else
+	echo "Making aarch64 build of citron..."
+	ARCH_FLAGS="-march=armv8-a -mtune=generic -O3"
+fi
+
+if [ "$DEVEL" = 'true' ]; then
+	citronpkg=citron-git
+	echo "Making nightly build..."
+else
+	citronpkg=citron
+	echo "Making stable build..."
+fi
+
+git clone https://aur.archlinux.org/"$citronpkg".git ./citron
+cd ./citron
+
+sed -i \
+	-e "s|x86_64|$ARCH|g"                              \
+	-e 's|DISCORD_PRESENCE=ON|DISCORD_PRESENCE=OFF|'   \
+	-e 's|USE_QT_MULTIMEDIA=ON|USE_QT_MULTIMEDIA=OFF|' \
+	-e 's|BUILD_TYPE=None|BUILD_TYPE=Release|'         \
+	-e "s|\$CXXFLAGS|$ARCH_FLAGS|g"                    \
+	-e "s|\$CFLAGS|$ARCH_FLAGS|g"                      \
+	./PKGBUILD
+cat ./PKGBUILD
+
+makepkg -fs --noconfirm --skippgpcheck
+ls -la .
+pacman --noconfirm -U ./*.pkg.tar.*
+pacman -Q "$citronpkg" | awk '{print $2; exit}' > ~/version
