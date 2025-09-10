9cbe344bf30a9bd8e28e853d94c087b74cb1e6c6
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-09-09 22:59:32 -0400

    mmm

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -57,4 +57,4 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-common intel-media-driver
+./get-debloated-pkgs.sh --add-mesa
