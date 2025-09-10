da02b379688b5454b7d04fc8a514feea21219531
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-09-10 01:10:39 -0400

    mmm

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -47,7 +47,7 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-mesa qt6-base-mini llvm-libs-nano
+./get-debloated-pkgs.sh --add-mesa qt6-base-mini llvm-libs-nano opus-nano
 
 echo "Building citron..."
 echo "---------------------------------------------------------------"
