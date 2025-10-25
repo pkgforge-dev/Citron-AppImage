55bec629133187b37f4178c129e6110635f1e6ec
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-10-24 21:38:13 -0400

    use common packages

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -48,7 +48,7 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-mesa qt6-base-mini llvm-libs-nano opus-nano gdk-pixbuf2-mini
+./get-debloated-pkgs.sh --add-common ffmpeg-mini intel-media-driver-mini
 
 echo "Building citron..."
 echo "---------------------------------------------------------------"
